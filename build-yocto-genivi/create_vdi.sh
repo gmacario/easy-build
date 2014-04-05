07b5c04368838b55ab796953036d79ab6d2b19fa
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2014-04-05 17:27:49 +0200

    Still debugging grub-install

--- build-yocto-genivi/create_vdi.sh
+++ build-yocto-genivi/create_vdi.sh
@@ -9,9 +9,9 @@
 
 # Prerequisites:
 #    qemu-img
+#    parted
 #    kpartx
 #    grub-install
-#    fdisk
 #    sudo
 
 TOPDIR=$PWD/tmp/build-gemini-5.0.2-qemux86
@@ -34,39 +34,38 @@ set -e
 
 qemu-img create -f raw $RAW_IMAGE 256M
 
-fdisk $RAW_IMAGE <<END
-n
-p
-1
+# Create partition table and partitions on RAW_IMAGE
+parted $RAW_IMAGE mklabel msdos
+parted $RAW_IMAGE print free
+parted $RAW_IMAGE mkpart primary ext3 1 220
+parted $RAW_IMAGE set 1 boot on
+parted $RAW_IMAGE print free
 
-
-a
-1
-p
-w
-END
-
+#echo "DBG: Checking $RAW_IMAGE:"
 #sfdisk -l $RAW_IMAGE
-fdisk -l $RAW_IMAGE
+#fdisk -l $RAW_IMAGE
 
 TMPFILE1=/tmp/kpartx-$$.tmp
 
 # See http://stackoverflow.com/questions/1419489/loopback-mounting-individual-partitions-from-within-a-file-that-contains-a-parti
 sudo kpartx -v -a $RAW_IMAGE >$TMPFILE1
 
-#echo "DBG: Contents of $TMPFILE1:"
-#cat $TMPFILE1
+echo "DBG: Contents of $TMPFILE1:"
+cat $TMPFILE1
 
-# loop0p1
-ROOTPART=`cut -d' ' -f3 $TMPFILE1`
+ROOTPART=/dev/mapper/`cut -d' ' -f3 $TMPFILE1`
 BLOCKDEV=`cut -d' ' -f8 $TMPFILE1`
 echo "DBG: ROOTPART=$ROOTPART"
 echo "DBG: BLOCKDEV=$BLOCKDEV"
 
-sudo mkfs -t ext3 /dev/mapper/$ROOTPART
+#echo "DBG: Checking $BLOCKDEV:"
+#sudo fdisk -l $BLOCKDEV
+
+sudo mkfs -t ext3 -L "GENIVI" $ROOTPART
 
 mkdir -p $MNT_ROOTFS
-sudo mount -o loop /dev/mapper/$ROOTPART $MNT_ROOTFS
+#sudo mount -o loop $ROOTPART $MNT_ROOTFS
+sudo mount $ROOTPART $MNT_ROOTFS
 
 TMPFILE2=/tmp/losetup-$$.tmp
 
@@ -86,7 +85,32 @@ sudo install -m644 -o 0 -v $KERNEL $MNT_ROOTFS/boot
 # TODO: Create grub.cfg to MNT_ROOTFS/boot/grub
 # grub-mkimage ???
 
-sudo grub-install --boot-directory=$MNT_ROOTFS/boot $BLOCKDEV
+# Create simple /boot/grub/grub.cfg on $ROOTPART
+# See http://www.linuxfromscratch.org/lfs/view/development/chapter08/grub.html
+
+TMPFILE3=/tmp/grubcfg-$$.tmp
+cat > $TMPFILE3 <<END
+# Begin /boot/grub/grub.cfg
+set default=0
+set timeout=5
+
+insmod ext2
+set root=(hd0,1)
+
+menuentry "Yocto-GENIVI, Linux" {
+        linux   /boot/bzImage-qemux86.bin root=/dev/sda1
+}
+
+#menuentry "GNU/Linux, Linux 3.13.6-lfs-SVN-20140404" {
+#        linux   /boot/vmlinuz-3.13.6-lfs-SVN-20140404 root=/dev/sda2 ro
+#}
+END
+
+sudo mkdir -p $MNT_ROOTFS/boot/grub
+sudo grub-mkdevicemap -m $MNT_ROOTFS/boot/grub/device.map
+#grub-probe $RAW_IMAGE
+#sudo install -m644 -o 0 -v $TMPFILE3 $MNT_ROOTFS/boot/grub/grub.cfg
+sudo grub-install --force --root-directory=$MNT_ROOTFS $BLOCKDEV || true
 
 echo "DBG: Contents of $MNT_ROOTFS:"
 ls -la $MNT_ROOTFS
@@ -103,17 +127,28 @@ sudo umount $MNT_ROOTFS
 
 sudo kpartx -d $RAW_IMAGE
 
-qemu-img convert -f raw -O vdi $RAW_IMAGE $VDI_IMAGE
-
-# TODO: Test: Run QEMU against VDI_IMAGE
-# TODO: Test: Run VirtualBox against VDI_IMAGE
-
 sudo losetup -av
-
-# See also: http://libguestfs.org/
 
 rm $TMPFILE1
 rm $TMPFILE2
+rm $TMPFILE3
+
+echo "DBG: Checking $RAW_IMAGE:"
+parted $RAW_IMAGE print free
+
+qemu-img convert -f raw -O vdi $RAW_IMAGE $VDI_IMAGE
+
+# TODO: Test: Run QEMU against VDI_IMAGE
+echo "TODO:" qemu-system-i386 -hda $RAW_IMAGE
+
+# TODO: Test: Run VirtualBox against VDI_IMAGE
+
+# TODO: Understand why the following error is shown when starting VM:
+#
+# error: no such device: d2033abb-85c3-47b2-81b3-59bdd07d7007
+# grub rescue>
+
+# See also: http://libguestfs.org/
 
 exit 0;
 
