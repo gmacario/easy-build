ab064197296ebc440c08784d553c658de2c51f80
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2015-04-30 15:39:56 +0200

    Run qemu in nographic mode is no DISPLAY set
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/run_gdp_qemu.sh
+++ build-yocto-genivi/run_gdp_qemu.sh
@@ -5,10 +5,15 @@ set -x
 export SHARED=$(dirname $(readlink -f $0))/shared
 source ${SHARED}/sources/poky/oe-init-build-env ${SHARED}/my-gdp-build03
 
-cd ${BUILDDIR}/tmp/deploy/images/qemux86-64
-runqemu \
-bzImage-qemux86-64.bin \
-genivi-demo-platform-qemux86-64.ext3 \
-qemuparams="-soundhw ac97"
+if [ "$DISPLAY" = "" ]; then
+    echo "WARNING: No DISPLAY set, running in nographic mode"
+    runqemu qemux86-64 tmp/deploy/images/qemux86-64/genivi-demo-platform-qemux86-64.ext3 nographic
+else
+    echo "DEBUG: DISPLAY=$DISPLAY, running in graphic mode"
+    # VGA graphics (only works from Ubuntu desktop)
+    export QEMU_AUDIO_DRV=pa
+    cd ${BUILDDIR}/tmp/deploy/images/qemux86-64 && runqemu bzImage-qemux86-64.bin \
+    genivi-demo-platform-qemux86-64.ext3 qemuparams="-soundhw ac97"
+fi
 
 # EOF
