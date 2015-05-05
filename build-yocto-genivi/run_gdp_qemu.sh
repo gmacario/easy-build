9ddeb33fed2c65db5101032ceab7ca43bdc45f6e
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2015-05-05 11:06:24 +0200

    Use runqemu from meta-genivi-demo/scripts
    See latest changes to instructions posted at
    http://wiki.projects.genivi.org/index.php/Hardware_Setup_and_Software_Installation/qemux86-64
    
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/run_gdp_qemu.sh
+++ build-yocto-genivi/run_gdp_qemu.sh
@@ -3,7 +3,7 @@
 #set -x
 
 export SHARED=$(dirname $(readlink -f $0))/shared
-source ${SHARED}/sources/poky/oe-init-build-env ${SHARED}/my-gdp-build03
+source ${SHARED}/sources/poky/oe-init-build-env ${SHARED}/my-gdp-build04
 
 if [ "$DISPLAY" = "" ]; then
     echo "WARNING: No DISPLAY set, running in nographic mode"
@@ -12,8 +12,11 @@ else
     echo "DEBUG: DISPLAY=$DISPLAY, running in graphic mode"
     # VGA graphics (only works from Ubuntu desktop)
     export QEMU_AUDIO_DRV=pa
-    cd ${BUILDDIR}/tmp/deploy/images/qemux86-64 && runqemu bzImage-qemux86-64.bin \
-    genivi-demo-platform-qemux86-64.ext3 qemuparams="-soundhw ac97"
+    cd ${BUILDDIR}/tmp/deploy/images/qemux86-64
+    ${SHARED}/sources/meta-genivi-demo/scripts/runqemu \
+        bzImage-qemux86-64.bin \
+        genivi-demo-platform-qemux86-64.ext3 \
+        qemuparams="-soundhw ac97"
 fi
 
 # EOF
