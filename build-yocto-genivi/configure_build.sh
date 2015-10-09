a35280048d062d5c29a7fa124ce63ab4b1a7ae60
Gianpaolo Macario <gmacario@gmail.com>
2015-10-09 10:12:27 +0200

    Discontinue YGB releases before 6.0.2

--- build-yocto-genivi/configure_build.sh
+++ build-yocto-genivi/configure_build.sh
@@ -63,31 +63,6 @@ cd $GENIVI/meta-openembedded    && git fetch --all && git checkout 5b0305d9efa4b
 #cd $GENIVI/meta-ivi && git checkout 6.0.2
 #cd $GENIVI/poky     && git checkout 8e05d5e3fe04face623c4f9fb08b12f13c22edab
 
-# Known SHA for Yocto GENIVI Baseline 6.0.1
-# See http://git.yoctoproject.org/cgit/cgit.cgi/meta-ivi/tree/README.md?id=6.0.1
-#cd $GENIVI/meta-ivi && git checkout 6.0.1
-#cd $GENIVI/poky     && git checkout bf8dcb43432004328162ddad3c8b38eaab6ab5ce
-
-# Known SHA for Yocto GENIVI Baseline 6.0.0
-# See http://git.yoctoproject.org/cgit/cgit.cgi/meta-ivi/tree/README.md?id=6.0.0
-#cd $GENIVI/meta-ivi && git checkout 6.0.0
-#cd $GENIVI/poky     && git checkout bf8dcb43432004328162ddad3c8b38eaab6ab5ce
-
-# Known SHA for Yocto GENIVI Baseline 5.0.2
-# See http://git.yoctoproject.org/cgit/cgit.cgi/meta-ivi/tree/README.md?id=5.0.2
-#cd $GENIVI/meta-ivi && git checkout 5.0.2
-#cd $GENIVI/poky     && git checkout 53d2563ff13fcec74d4250bef5419e36169e55cc
-
-# Known SHA for Yocto GENIVI Baseline 5.0.1
-# See http://git.yoctoproject.org/cgit/cgit.cgi/meta-ivi/tree/README.md?id=5.0.1
-#cd $GENIVI/meta-ivi && git checkout 5.0.1
-#cd $GENIVI/poky     && git checkout 53d2563ff13fcec74d4250bef5419e36169e55cc
-
-# Known SHA for Yocto GENIVI Baseline 5.0.0
-# See http://git.yoctoproject.org/cgit/cgit.cgi/meta-ivi/tree/README.md?id=5.0.0
-#cd $GENIVI/meta-ivi && git checkout 5.0.0
-#cd $GENIVI/poky     && git checkout 56f39bcf2237c11508d82238b6292e4bfbfed764
-
 cd $TOPDIR
 
 cp $TOPDIR/conf/bblayers.conf $TOPDIR/conf/bblayers.conf.ORIG
