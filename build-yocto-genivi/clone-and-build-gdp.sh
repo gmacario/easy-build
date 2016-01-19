bf7f580357ca95b33c0271bfd0980dbe629461b0
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-01-19 15:32:25 +0100

    Add default git configuration
    See http://lists.genivi.org/pipermail/genivi-projects/2016-January/001311.html
    
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/clone-and-build-gdp.sh
+++ build-yocto-genivi/clone-and-build-gdp.sh
@@ -9,6 +9,9 @@ GDP_URL=git://git.projects.genivi.org/genivi-demo-platform.git
 GDP_BRANCH=qemux86-64
 WORKDIR=genivi-demo-platform
 
+git config --global user.name "easy-build"
+git config --global user.email "$(whoami)@$(hostname)"
+
 if [ ! -e $WORKDIR ]; then git clone -b $GDP_BRANCH $GDP_URL $WORKDIR; fi
 cd $WORKDIR && git fetch --all --prune
 source init.sh
