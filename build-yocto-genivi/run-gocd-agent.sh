8f1c4e4107b2552d546181289020375c25982176
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-01-24 09:38:38 +0100

    Add support for implementing a gocd agent
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

---
+++ build-yocto-genivi/run-gocd-agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run as a GoCD Agent
+
+set -x
+set -e
+
+# TODO
+
+git config --global user.name "easy-build"
+git config --global user.email "$(whoami)@$(hostname)"
+
+# TODO
+
+# EOF
