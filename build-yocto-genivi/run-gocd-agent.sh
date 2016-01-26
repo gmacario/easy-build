b687a67cfb6c3a409c3e4ecf437049e6390078b9
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-01-26 12:04:06 +0100

    Add example of usage
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/run-gocd-agent.sh
+++ build-yocto-genivi/run-gocd-agent.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+# ===========================================================================
+# Project: gmacario/easy-build
+#
 # Run as a GoCD Agent
+#
+# Example of usage
+#
+# $ docker run -d --user=go \
+#   --hostname my-genivigo-testagent \
+#   --workdir /var/lib/go-agent \
+#   gmacario/build-yocto-genivi /usr/local/bin/run-gocd-agent.sh
+# ===========================================================================
 
 # set -x
 set -e
