7b384f4b8be8c0f8f089e76c2e54f49a5b2600c4
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-01-26 11:16:24 +0100

    Configure and start go-agent
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/run-gocd-agent.sh
+++ build-yocto-genivi/run-gocd-agent.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 # Run as a GoCD Agent
 
 set -x
 set -e
 
-# TODO
-
 git config --global user.name "easy-build"
 git config --global user.email "$(whoami)@$(hostname)"
 
-# TODO
+cp /etc/default/go-agent go-agent.ORIG
+sed 's/^GO_SERVER=.*$/GO_SERVER=go.genivi.org/g' go-agent.ORIG >/etc/default/go-agent
+service go-agent start
 
 # EOF
