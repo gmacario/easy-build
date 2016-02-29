d59de5c2b02bf70f7bcee14803aa673433efa947
Gianpaolo Macario <gmacario@gmail.com>
2016-02-29 18:33:53 +0100

    clone-and-build-gdp: Build given GDP_SHA if defined
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- build-yocto-genivi/clone-and-build-gdp.sh
+++ build-yocto-genivi/clone-and-build-gdp.sh
@@ -7,13 +7,20 @@ set -e
 
 GDP_URL=git://git.projects.genivi.org/genivi-demo-platform.git
 GDP_BRANCH=qemux86-64
+# GDP_SHA=811273dd0984169605bf216046cdf3c9a88448c6
+
 WORKDIR=genivi-demo-platform
+[ "$GDP_SHA" != "" ] && WORKDIR=$WORKDIR-$GDP_SHA
 
 git config --global user.name "easy-build"
 git config --global user.email "$(whoami)@$(hostname)"
 
 if [ ! -e $WORKDIR ]; then git clone -b $GDP_BRANCH $GDP_URL $WORKDIR; fi
 cd $WORKDIR && git fetch --all --prune
+[ "$GDP_SHA" != "" ] && git checkout $GDP_SHA
+git show
+git status
+
 source init.sh
 bitbake genivi-demo-platform
 
